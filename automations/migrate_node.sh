1c8d78f218710cb9619183e52e988b47d3a3f8f2
tdorji <c-tenzin.dorji@charter.com>
2022-02-17 23:09:32 -0600

    add rollback option

--- automations/migrate_node.sh
+++ automations/migrate_node.sh
@@ -31,7 +31,7 @@ if [[ $action == upgrade ]];then
       $SSH_OPTION$server "sudo sed -i 's/^exclude=puppet-agent/#exclude=puppet-agent/g' /etc/yum.conf &&\
       sudo rm -rf /tmp/puppet-agent* &> /dev/null &&\
       yes|sudo cp -RP /etc/puppetlabs/ /tmp/ &&\
-      sudo wget --no-check-certificate --progress=bar:force -P /tmp/ https://vm0pncorexa0001.corp.chartercom.com:8140/packages/2019.8.3/el-7-x86_64/puppet-agent-6.19.1-1.el7.x86_64.rpm &&\
+      sudo wget --no-check-certificate --progress=bar:force -P /tmp/ https://new_pe_master:8140/packages/2019.8.3/el-7-x86_64/puppet-agent-6.19.1-1.el7.x86_64.rpm &&\
       sudo rpm -e puppet-agent &&\
       sudo rm -rf /etc/puppetlabs &> /dev/null &&\
       sudo rm -rf /opt/puppetlabs &> /dev/null &&\
@@ -39,7 +39,7 @@ if [[ $action == upgrade ]];then
       sudo /usr/local/bin/puppet --version &&\
       yes|sudo cp /tmp/puppetlabs/puppet/puppet.conf /etc/puppetlabs/puppet/ &&\
       yes|sudo cp -RP /tmp/puppetlabs/facter/ /etc/puppetlabs/ &&\
-      sudo sed -i 's/vm0pnpuppta0001.twcable.com/vm0pncorexa0001.corp.chartercom.com/g' /etc/puppetlabs/puppet/puppet.conf &&\
+      sudo sed -i 's/old_pe_master/new_pe_master/g' /etc/puppetlabs/puppet/puppet.conf &&\
       sudo sed -i 's/${current_branch}/${new_branch}/g' /etc/puppetlabs/puppet/puppet.conf &&\
       echo "Puppet Agent installed" &&\
       sudo rm -rf /opt/puppetlabs/puppet/cache/state/agent_catalog_run.lock &> /dev/null" 1>> $home/tmp/migrate.log 2>> $home/tmp/migrate.log
@@ -53,7 +53,7 @@ elif [[ $action == rollback ]];then
       count=`expr $count + 1`
       echo "${count}:${server}:Rolling back ..." >> $home/tmp/migrate.log
       $SSH_OPTION$server "sudo rm -rf /tmp/puppet-agent* &> /dev/null &&\
-      sudo wget --no-check-certificate --progress=bar:force -P /tmp/ https://vm0pnpuppta0001.twcable.com:8140/packages/2018.1.2/el-7-x86_64/puppet-agent-5.5.3-1.el7.x86_64.rpm &&\
+      sudo wget --no-check-certificate --progress=bar:force -P /tmp/ https://old_pe_master:8140/packages/2018.1.2/el-7-x86_64/puppet-agent-5.5.3-1.el7.x86_64.rpm &&\
       sudo rpm -e puppet-agent &&\
       sudo rpm -i /tmp/puppet-agent-5.5.3-1.el7.x86_64.rpm &&\
       sudo /usr/local/bin/puppet --version &&\
